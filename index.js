9779fdf5ed5c5d6e9b509ba5e9044ea573695a02
arukusays <syasukura@gmail.com>
2024-03-21 10:18:04 +0900

    複数ファイルの入出力に対応

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 console.log('start!');
 import Handlebars from "handlebars";
 import Fs from "fs";
+import path from "path";
 
-let templateFile = Fs.readFileSync('ticket.hds','utf-8');
+const targetDir = './sample';
+const outputDir = './output';
+
+let templateFile = Fs.readFileSync('ticket.hds', 'utf-8');
 let template = Handlebars.compile(templateFile);
 
-let dataText = Fs.readFileSync('sample.json', 'utf-8');
-let data = JSON.parse(dataText);
-console.log(template(data));
-let outputFileName = `${data.id}_${data.title}.md`;
-Fs.writeFileSync(outputFileName, template(data));
+// 対象ディレクトリからJSONファイル一覧を抽出する.
+console.log(`target directory: ${targetDir}.`);
+const files = Fs.readdirSync(targetDir)
+    .filter((file) => path.extname(file).toLowerCase() === '.json');
+console.log(`target json: ${files.length} files.`);
 
-console.log('end!!');
+// JSONファイルを１つずつ読み込み、mdファイルを出力していく.
+let errorFiles = 0;
+files.forEach((file) => {
+    console.log(`target: ${file}`);
+    try{
+        let dataText = Fs.readFileSync(`${targetDir}/${file}`, 'utf-8');
+        let data = JSON.parse(dataText);
+        let outputFileName = `${data.id}_${data.title}.md`;
+        Fs.writeFileSync(`${outputDir}/${outputFileName}`, template(data));
+    }catch(e){
+        errorFiles++;
+        console.log(`error occured at ${file}.`);
+        console.log(e);
+    }
+});
+
+console.log(`finished. error files: ${errorFiles}.`);
